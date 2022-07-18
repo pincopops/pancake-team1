c023896d204ed3a294e145ced051e10bae28950a
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-18 16:53:31 +0200

    separatorMaker

--- app.js
+++ app.js
@@ -863,23 +863,58 @@ const arr = ["ciao", "bye", "ola"];
 
 const newTab = new Vercialtab(arr);
 
-//*************************************************************
+//**************************************************************/
 
-let tokenArr = [];
+// let tokenArr = [];
 
-const binancePublicEndpoint = "https://api.binance.com";
-const exchangeInfoEndpoint = binancePublicEndpoint + "/api/v3/exchangeInfo";
-const tickersEndpoint = binancePublicEndpoint + "/api/v3/ticker/price";
+// const binancePublicEndpoint = "https://api.binance.com";
+// const exchangeInfoEndpoint = binancePublicEndpoint + "/api/v3/exchangeInfo";
+// const tickersEndpoint = binancePublicEndpoint + "/api/v3/ticker/price";
 
-componentDidMount = async () => {
-	const resToken = await fetch("https://api.pancakeswap.info/api/v2/pairs");
-	const dataToken = await resToken.json();
+// componentDidMount = async () => {
+// 	const resToken = await fetch("https://api.pancakeswap.info/api/v2/pairs");
+// 	const dataToken = await resToken.json();
 
-	const tokens = Object.values(dataToken.data);
+// 	const tokens = Object.values(dataToken.data);
 
-	return tokens;
-};
+// 	return tokens;
+// };
 
-componentDidMount().then((tokens) => {
-	tokens.forEach((token) => console.log(token));
-});
+// componentDidMount().then((tokens) => {
+// 	tokens.forEach((token) => console.log(token));
+// });
+
+//******************************************************************** */
+
+const sections = document.querySelectorAll('div')
+
+
+const sectionsArr = Array.from(sections)
+
+const onlysection = sectionsArr.filter(section => section.dataset.id === 'section')
+
+console.log(onlysection);
+
+
+separatorMaker = (array,imgurl,sec) => {
+
+	array.find(section => {
+
+		if(section.classList.contains(sec)){
+		const separatorWrapper = document.createElement('div')
+		separatorWrapper.classList.add('separator-wrapper')
+		const img = document.createElement('img')
+		img.classList.add('separator-img')
+		img.setAttribute("src",imgurl)
+		separatorWrapper.appendChild(img)
+		section.appendChild(separatorWrapper)}
+	})
+}
+
+separatorMaker(onlysection,"./assets/asset 47.svg","wrapper-typography")
+separatorMaker(onlysection,"./assets/asset 2.png","colors-container")
+separatorMaker(onlysection,"./assets/asset 0.png","wrapper-icons")
+separatorMaker(onlysection,"./assets/asset 6.png","wrapper-form-tab")
+separatorMaker(onlysection,"./assets/asset 8.png","wrapper-button")
+separatorMaker(onlysection,"./assets/asset 29.png","wrapper-tab")
+separatorMaker(onlysection,"./assets/asset 13.png","wrapper-card")

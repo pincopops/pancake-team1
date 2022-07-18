daf7bbc8541d0b901ded7a7c8d64a4bd0fddfac3
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-18 09:37:27 +0200

    vertical tab almost done

--- app.js
+++ app.js
@@ -623,6 +623,8 @@ class Input {
 		const InputProperties = document.createElement("div");
 		InputProperties.classList.add(`input-${this.name}-properties`);
 		InputProperties.innerHTML = `<h4>PROPERTIES</h4>`;
+
+	
 		Object.entries(this.properties).forEach((el) => {
 			const elContainer = document.createElement("div");
 			elContainer.classList.add(`input-${this.name}-properties-unit`);
@@ -695,6 +697,8 @@ document.querySelector("[type=checkbox]").classList.add("checkbox-input");
  */
 
 const verticalTabWrapper = document.querySelector(".wrapper-tab-vertical");
+verticalTabWrapper.innerHTML = "<h1>Vertical Tab</h1>"
+
 
 class Vercialtab {
 
@@ -738,7 +742,90 @@ class Vercialtab {
 			}
 		})
 
+		tabContent.addEventListener("click", (item,i) => {
+			tabHeader.innerHTML = `<span>${item.target.textContent}</span>`
+			tabContent.classList.add("hide")
+			tabHeader.style.borderRadius = "0.8rem 0.8rem 0.8rem 0.8rem"
+			tabHeader.appendChild(triangle)
+			
+		})
+
+		const infoWrapper = document.createElement("div")
+		infoWrapper.classList.add("infoWrapper")
+
+		const propertiesEl = document.createElement("div")
+		propertiesEl.classList.add(`${this.name}-properties`)
+		propertiesEl.innerHTML = `<h4>PROPERTIES:</h4>`
+		verticalTabWrapper.appendChild(infoWrapper)
+		infoWrapper.appendChild(propertiesEl)
+
+		Object.entries(this.properties).forEach(el => {
+			console.log(el)
+			const elWrapper = document.createElement("div");
+			elWrapper.classList.add(`input-${this.name}-properties-unit`);
+			el.map((el) => {
+				const elContainer = document.createElement("div");
+				elContainer.innerText = `${el}`;
+				elWrapper.appendChild(elContainer);
+			});
+			propertiesEl.appendChild(elWrapper);
+		})
+
+		const typefaceEl = document.createElement("div")
+		typefaceEl.classList.add(`${this.name}-typeface`)
+		typefaceEl.innerHTML = `<h4>TYPEFACE:</h4>`
+		infoWrapper.appendChild(typefaceEl)
+		
+		Object.entries(this.typeface).forEach(el => {
+			console.log(el)
+			const elWrapper = document.createElement("div");
+			elWrapper.classList.add(`input-${this.name}-typeface-unit`);
+			el.map((el) => {
+				const elContainer = document.createElement("div");
+				elContainer.innerText = `${el}`;
+				elWrapper.appendChild(elContainer);
+			});
+			typefaceEl.appendChild(elWrapper);
+		})
+
+		const colorEl = document.createElement("div")
+		colorEl.classList.add(`${this.name}-colors`)
+		colorEl.innerHTML = `<h4>COLORS:</h4>`
+		infoWrapper.appendChild(colorEl)
+
+		Object.entries(this.colors).forEach(el => {
+			console.log(el)
+			const elWrapper = document.createElement("div");
+			elWrapper.classList.add(`input-${this.name}-typeface-unit`);
+			el.map((el) => {
+				const elContainer = document.createElement("div");
+				elContainer.innerText = `${el}`;
+				elWrapper.appendChild(elContainer);
+			});
+			colorEl.appendChild(elWrapper);
+		})
+
+		const effectEl = document.createElement("div")
+		effectEl.classList.add(`${this.name}-effects`)
+		effectEl.innerHTML = `<h4>EFFECT:</h4>`
+		infoWrapper.appendChild(effectEl)
+
+		Object.entries(this.effect.hover).forEach(el => {
+			console.log(el)
+			const elWrapper = document.createElement("div");
+			elWrapper.classList.add(`input-${this.name}-typeface-unit`);
+			el.map((el) => {
+				const elContainer = document.createElement("div");
+				elContainer.innerText = `${el}`;
+				elWrapper.appendChild(elContainer);
+			});
+			effectEl.appendChild(elWrapper);
+		})
+
+
 	}
+
+	name = "verticalTab"
 
 	properties = {
 		padding: "8px 16px",
@@ -763,6 +850,7 @@ class Vercialtab {
 			backgroundColor : "#262130" 
 		}
 	}
+
 
 }
 

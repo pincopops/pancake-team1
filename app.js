5eab42ec2ec1550ed6322106eabe280b231725d3
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-18 13:00:16 +0200

    fix dinamic adding colors

--- app.js
+++ app.js
@@ -24,59 +24,59 @@ class Color {
 		colorUnitWrapper.classList.add("wrapper-colors-unit")
 		colorUnitWrapper.append(itemPalette, itemInfo);
 		const wrapperColors = document.querySelector(".wrapper-colors");
-		wrapperColors.append(colorUnitWrapper);
+		wrapperColors.appendChild(colorUnitWrapper);
 	}
 
 }
 
 const colorsArr = [
-	new Color("$primary", "#666171"),
-	new Color("$darkBlack", "#08060b"),
-	new Color("$blackBrand", "#191326"),
-	new Color("$lightBlack", "#222222"),
-	new Color("$borderBlack", "#262130"),
-	new Color("$darkGray3", "#383241"),
-	new Color("$darkGray2", "#3c3742"),
-	new Color("$darkGray1", "#27262c"),
-	new Color("$gray", "#999999"),
-	new Color("$lightGray", "#cccccc"),
-	new Color("$borderGray", "#565065"),
-	new Color("$violetBrand", "#7645d9"),
-	new Color("$violetBrand2", "#9a6aff"),
-	new Color("$lightViolet", "#f4eeff"),
-	new Color("$lightViolet2", "#b8add2"),
-	new Color("$subTitleViolet", "#7a6eaa"),
-	new Color("$blueBrand", "#1155cc"),
-	new Color("$darkBlue", "#280d5f"),
-	new Color("$darkBlue2", "#3c1786"),
-	new Color("$tabBlue", "#372f47"),
-	new Color("$hoverMenu", "#353547"),
-	new Color("$pinkBrand", "#ed4b9e"),
-	new Color("$ocra", "#ffb237"),
-	new Color("$whiteBrand", "#ffffff"),
-	new Color("$tiffanyBrand", "#1fc7d4"),
-	new Color("$greenBrand", "#29910d"),
-	new Color("$trasparency", "#00000000"),
-	new Color("$hoverEffect", "#08060b00"),
+	new Color("$primary", "#666171").colorMaker(),
+	new Color("$darkBlack", "#08060b").colorMaker(),
+	new Color("$blackBrand", "#191326").colorMaker(),
+	new Color("$lightBlack", "#222222").colorMaker(),
+	new Color("$borderBlack", "#262130").colorMaker(),
+	new Color("$darkGray3", "#383241").colorMaker(),
+	new Color("$darkGray2", "#3c3742").colorMaker(),
+	new Color("$darkGray1", "#27262c").colorMaker(),
+	new Color("$gray", "#999999").colorMaker(),
+	new Color("$lightGray", "#cccccc").colorMaker(),
+	new Color("$borderGray", "#565065").colorMaker(),
+	new Color("$violetBrand", "#7645d9").colorMaker(),
+	new Color("$violetBrand2", "#9a6aff").colorMaker(),
+	new Color("$lightViolet", "#f4eeff").colorMaker(),
+	new Color("$lightViolet2", "#b8add2").colorMaker(),
+	new Color("$subTitleViolet", "#7a6eaa").colorMaker(),
+	new Color("$blueBrand", "#1155cc").colorMaker(),
+	new Color("$darkBlue", "#280d5f").colorMaker(),
+	new Color("$darkBlue2", "#3c1786").colorMaker(),
+	new Color("$tabBlue", "#372f47").colorMaker(),
+	new Color("$hoverMenu", "#353547").colorMaker(),
+	new Color("$pinkBrand", "#ed4b9e").colorMaker(),
+	new Color("$ocra", "#ffb237").colorMaker(),
+	new Color("$whiteBrand", "#ffffff").colorMaker(),
+	new Color("$tiffanyBrand", "#1fc7d4").colorMaker(),
+	new Color("$greenBrand", "#29910d").colorMaker(),
+	new Color("$trasparency", "#00000000").colorMaker(),
 	new Color("$hoverEffect", "#08060b00").colorMaker(),
+	// new Color("$hoverEffect", "#08060b00").colorMaker(),
 	new Color("$hoverEffect", "#ffb237").colorMaker(),
 ];
 
 const unitColor = document.querySelectorAll(".wrapper-colors-unit");
 
-unitColor.forEach((item, i) => {
-	const itemPalette = item.querySelector(".wrapper-colors-unit-palette");
-	itemPalette.style.backgroundColor = colorsArr[i].hex;
+// unitColor.forEach((item, i) => {
+// 	const itemPalette = item.querySelector(".wrapper-colors-unit-palette");
+// 	itemPalette.style.backgroundColor = colorsArr[i].hex;
 
-	const itemInfo = item.querySelector(".wrapper-colors-unit-info");
-	itemInfo.innerHTML = `<p>${colorsArr[i].name}</p>
-    <p>${colorsArr[i].hex}</p>`;
+// 	const itemInfo = item.querySelector(".wrapper-colors-unit-info");
+// 	itemInfo.innerHTML = `<p>${colorsArr[i].name}</p>
+//     <p>${colorsArr[i].hex}</p>`;
 
-	itemPalette.addEventListener("click", () => {
-		navigator.clipboard.writeText(colorsArr[i].hex);
-		alert("HEX copiato negli appunti: " + colorsArr[i].hex);
-	});
-});
+// 	itemPalette.addEventListener("click", () => {
+// 		navigator.clipboard.writeText(colorsArr[i].hex);
+// 		alert("HEX copiato negli appunti: " + colorsArr[i].hex);
+// 	});
+// });
 
 const colorsWrapperEl = document.querySelector(".colors-container");
 const colorsTitle = document.createElement("h1");

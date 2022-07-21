122d5077467230cb097e966fc979693dfab86b87
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-07-21 10:17:49 +0200

    feat : add banner

--- appDesignSystem/banner.mjs
+++ appDesignSystem/banner.mjs
@@ -1,5 +1,5 @@
 
-export const countDownMaker = (dateCountDown, containerId) => {
+const countDownMaker = (dateCountDown, containerId) => {
 
   var countDownDate = new Date(dateCountDown).getTime();
 
@@ -28,41 +28,71 @@ export const countDownMaker = (dateCountDown, containerId) => {
 
 
 
-export const lotteryCount = (initNumber) => {
 
-  var x = setInterval(() => {
-    initNumber += ((initNumber * 1) / 100)
+  const lotteryCount = (initNumber) => setInterval(() => {
+    initNumber += ((initNumber * 1) / 100);
     //document.getElementById(containerId).innerHTML = initNumber;
-    const lotteryTitle = document.createElement('h2')
+    const lotteryTitle = document.createElement('h2');
 
-    lotteryTitle.innerText = "win $" + Math.round(initNumber) + "  in Lottery"
-    return lotteryTitle
+    lotteryTitle.innerText = "win $" + Math.round(initNumber) + "  in Lottery";
+
+    console.log(initNumber);
+
+
+    return initNumber;
+
+
   }, 3000)
 
 
-}
+
+//lotteryCount(5000)
 
 
 
-
-export const bannerMaker = (containerId, countDownMaker, lotteryCount) => {
+export const bannerMaker = (containerId,) => {
 
   const container = document.getElementById(containerId)
 
   const lotteryBannerContainer = document.createElement('div')
 
+  lotteryBannerContainer.classList.add('lottery-banner')
+
   const lotteryTitle = document.createElement('h2')
 
-  lotteryTitle.innerText = "win $ " + lotteryCount + " in Lottery"
+  lotteryTitle.classList.add('lottery-title')
 
+  lotteryTitle.innerText = "win $5000 in Lottery"
 
   const lotteryCounter = document.createElement('div')
 
+  lotteryCounter.classList.add('lottery-counter')
+
+  const lotteryCounterTitle = document.createElement('h2')
+
+  lotteryCounterTitle.classList.add('lottery-counter-title')
+
+  lotteryCounterTitle.innerText = "0h 0m 0s"
+
+  lotteryCounter.appendChild(lotteryCounterTitle)
+
   lotteryCounter.setAttribute("id", "lotteryCounter")
+
+  const lotteryCounterBackground = document.createElement('img')
+
+  lotteryCounterBackground.src = "./assets/asset 37.png"
+
+  lotteryCounterBackground.classList.add('lottery-counter-background')
+
+  lotteryCounter.appendChild(lotteryCounterBackground)
 
   const lotteryButton = document.createElement('div')
 
+  lotteryButton.classList.add('lottery-button')
+
   const lotteryBannerContainerContent = document.createElement('div')
+
+  lotteryBannerContainerContent.classList.add('lottery-banner-content')
 
   lotteryBannerContainerContent.append(lotteryTitle, lotteryCounter, lotteryButton)
 
@@ -70,9 +100,38 @@ export const bannerMaker = (containerId, countDownMaker, lotteryCount) => {
 
   container.appendChild(lotteryBannerContainer)
 
+  const lotteryImageContainer = document.createElement('div')
+
+  lotteryImageContainer.classList.add('lottery-image-container')
+
+  const lotteryImageContainerBackground = document.createElement('img')
+
+  lotteryImageContainerBackground.src = "./assets/asset 37.png"
+
+  lotteryImageContainerBackground.classList.add('lottery-image-container-background')
+
+  lotteryImageContainer.appendChild(lotteryImageContainerBackground)
+
+  const lotteryImage = document.createElement('img')
+
+  lotteryImage.classList.add('lottery-image')
+
+  lotteryImage.src = "./assets/asset 2.png"
+
+  lotteryImageContainer.appendChild(lotteryImage)
+
+  lotteryBannerContainer.appendChild(lotteryImageContainer)
+
+
+
+
+
+
+
+
 }
 
-bannerMaker("lotteryContainer", countDownMaker('july 21, 2022 01:22:00', "lotteryContainer"), lotteryCount(32740))
+bannerMaker("lotteryContainer")
 
 
 ;
